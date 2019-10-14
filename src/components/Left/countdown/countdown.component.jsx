6f7cf69378e76cc28d2ec45e4e2d08d6a8889271
DVitsme <derrick.valentine@gmail.com>
2019-10-14 13:41:22 -0400

    Completed Styling

--- src/components/Left/countdown/countdown.component.jsx
+++ src/components/Left/countdown/countdown.component.jsx
@@ -20,27 +20,45 @@ const Countdown = () => {
     width: 90%;
     display: flex;
     justify-content: center;
+    letter-spacing: 0.3em;
+    span {
+      padding: 0 20px;
+      font-size: 12px;
+      font-weight: 600;
+      div {
+        font-size: 40px;
+      }
+    }
+  `;
+  const Middle = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
   `;
   return (
     <CountdownCol className="col">
-      <Time className="middle">
-        <span>
-          <div id="days">12</div>
-          days
-        </span>
-        <span>
-          <div id="hours">06</div>
-          hours
-        </span>
-        <span>
-          <div id="minutes">35</div>
-          minutes
-        </span>
-        <span>
-          <div id="seconds">26</div>
-          seconds
-        </span>
-      </Time>
+      <Middle>
+        <Time>
+          <span>
+            <div id="days">12</div>
+            days
+          </span>
+          <span>
+            <div id="hours">06</div>
+            hours
+          </span>
+          <span>
+            <div id="minutes">35</div>
+            minutes
+          </span>
+          <span>
+            <div id="seconds">26</div>
+            seconds
+          </span>
+        </Time>
+      </Middle>
     </CountdownCol>
   );
 };
