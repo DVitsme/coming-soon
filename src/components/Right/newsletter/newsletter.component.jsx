8bbe95f2919c3c715257676df456f1db9e2f9444
DVitsme <derrick.valentine@gmail.com>
2019-10-15 18:04:55 -0400

    Does not send emails, instead stores them on firebase

--- src/components/Right/newsletter/newsletter.component.jsx
+++ src/components/Right/newsletter/newsletter.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import AddSubscriber from '../addSubscriber/addSubscriber.component';
 
 export default class Newsletter extends Component {
   constructor(props) {
@@ -12,16 +13,30 @@ export default class Newsletter extends Component {
     const NewsletterCol = styled.div`
       background: #fff;
     `;
+    const Middle = styled.div`
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+    `;
+    const NewsletterContainer = styled.div`
+      width: 90%;
+      h4 {
+        font-style: italic;
+        font-size: 12ppx;
+      }
+    `;
+
     return (
       <NewsletterCol className="col">
-        <div className="newsletter middle">
-          <h2>Valentine Consulting is coming soon</h2>
-          <h4>Subscribe to get a notification when we launch</h4>
-          <form action="">
-            <input type="text" placeholder="Enter Your Email" />
-            <input type="button" className="newsletter-btn" value="Subscribe" />
-          </form>
-        </div>
+        <Middle>
+          <NewsletterContainer>
+            <h2>Valentine Consulting is coming soon</h2>
+            <h4>Subscribe to get a notification when we launch</h4>
+            <AddSubscriber />
+          </NewsletterContainer>
+        </Middle>
       </NewsletterCol>
     );
   }
